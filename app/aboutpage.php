a996a7f59162c9d74f216e32b9394093e62a4713
Rabiuzzaman <rahatu666@gmail.com>
2017-12-27 22:58:27 +0600

    about adding

---
+++ app/aboutpage.php
@@ -0,0 +1,14 @@
+<body>
+	<header>
+		<center><a href="index.php"><h1>Kinbo.com</h1></a>
+		
+	</header>
+	<p align="center">
+		This website is in the purpose to sell mobiles in online.Customer can see,choose
+		and compare differences between various kinds of mobiles.They can easily order any product
+		we will give service.This project is to ease the buying for people who don't get time to go 
+		to market or physically disabled people.We wish everyone will like our website and support us.Thanks very 
+		much for spending time with us.
+	</p>	
+		
+</body>
