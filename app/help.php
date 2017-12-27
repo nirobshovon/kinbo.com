d19edf216a5779e6c82d653062e2944fc84857c5
Rabiuzzaman <rahatu666@gmail.com>
2017-12-27 16:50:42 +0600

    help page

---
+++ app/help.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<title>E-Commerse</title>
+</head>
+<body>
+			<fieldset>
+
+				<center><h1><a href="index.php">Kinbo.com</a></h1></center></br>
+
+				<table width="100%">
+					<tr>
+						<td align="center"><a href="slider.php">Home</a></td>
+						<td align="center"><a href="index.php">Products</a></td>
+						<td align="center"><a href="">About</a></td>
+						<td align="center"><a href="">Contact </a></td>
+						<td align="center"><a href="cart.php">Cart </a></td>
+						<td align="center"><a href="login.php">Login</a></td>
+					</tr>
+				</table></br></br></br>
+				</fieldset>
+<fieldset>				
+
+
+
+</fieldset>
+
+<fieldset>
+	<table border="1px" width="100%">
+		<tr > 
+			<td>
+				<h4>Customer Service</h4></br>
+				<a href="">Help center</a></br>
+				<a href="">How to shop on Kinbo.com</a></br>
+				<a href="">Track order</a></br>
+				<a href="">Why shop with Kinbo</a></br>
+				<a href="">returns & Refunds</a></br>
+				<a href="">Contact us</a></br>
+			</td>
+			
+			<td> 
+				<h4>About Us</h4></br>
+				<a href="">About Kinbo.com</a></br>
+				<a href="">Corporate Sales</a></br>
+				<a href="">Terms And Conditions</a></br>
+				<a href="">Privacy Policy</a></br>
+				<a href="">Careers</a></br>
+				
+			</td>
+			
+			<td>
+				<h4>Join Us</h4></br>
+				<a><img width="32px" height="32px" src="C:\xampp\htdocs\kinbo.com\image\facebook.png"></img>
+				</a>
+				<a><img width="32px" height="32px" src="C:\xampp\htdocs\kinbo.com\image\gmail.png"></img>
+				</a>
+				<a><img width="32px" height="32px" src="C:\xampp\htdocs\kinbo.com\image\twitter.png"></img>
+				</a>
+				
+			</td>
+			
+			
+		</tr>
+	</table>
+
+
+</fieldset>
+</body>
+</html>
