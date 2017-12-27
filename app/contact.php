cc5e54fe6b7c8ba611fab652bf6c54d411bde975
Rabiuzzaman <rahatu666@gmail.com>
2017-12-27 23:59:11 +0600

    contact adding and changes in other files

---
+++ app/contact.php
@@ -0,0 +1,14 @@
+<body>
+	<header>
+		<center><a href="slider.php"><h1>Kinbo.com</h1></a>
+		
+	</header>
+	<p align="center">
+		To contact</br>
+		#MD.MORSHAD SARWAR-01774486000 </br>
+		#MD.RABIUZZAMAN MAZUMDER-01670622994 </br>
+		#AKASH SHAHA-01837757678 </br>
+		
+	</p>	
+		<a href="slider.php" >back</a>
+</body>
