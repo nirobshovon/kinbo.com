684fbbf23a125ebf58330e24b763852fd2171f85
Rabiuzzaman <rahatu666@gmail.com>
2017-12-27 21:13:01 +0600

    user menu showing in left

---
+++ app/userLeftMenu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<title>E-Commerse</title>
+</head>
+<body>
+	<table border="1" width="100%">
+
+			<ul>
+				<tr>
+					<td>
+						<h3>Dashboard</h3>
+
+				<li>Orders
+				<ul>
+					<li><a href="userPendingOrder.php">Pending Orders</a></li>
+					<li><a href="userDeliveredOrder.php">Delivered Orders</a></li>
+				</ul>
+			</br></br><li><a href="cart.php" ><input type="button" value="Cart"/></a></li>
+		</br></br>	<li><a href="slider.php"><input type="button" value="Logout"/></a></li>
+			</ul>
+		</td>
+	</tr>
+</table>
+</body>
