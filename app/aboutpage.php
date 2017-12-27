b8324756e3d70817801f9006e79fdba20ce0181c
Sarwar Morshad <nirobshovon69@gmail.com>
2017-12-28 01:16:32 +0600

    About Modified

--- app/aboutpage.php
+++ app/aboutpage.php
@@ -1,14 +1,17 @@
 <body>
 	<header>
-		<center><a href="slider.php"><h1>Kinbo.com</h1></a>
-		
+		<center><a href="slider.php"><h1>Kinbo.com</h1></a></center>
+		<center>
+			<img src="C:\xampp\htdocs\kinbo.com\app\kinbo.jpg"></img>
+		</center>
+
 	</header>
-	<p align="center">
+	<h3><p align="center">
 		This website is in the purpose to sell mobiles in online.Customer can see,choose
 		and compare differences between various kinds of mobiles.They can easily order any product
-		we will give service.This project is to ease the buying for people who don't get time to go 
-		to market or physically disabled people.We wish everyone will like our website and support us.Thanks very 
+		we will give service.This project is to ease the buying for people who don't get time to go
+		to market or physically disabled people.We wish everyone will like our website and support us.Thanks very
 		much for spending time with us.
-	</p>	
-		<a href="slider.php" >back</a>
+	</p></h3>
+		<center><a href="slider.php" >Back</a></center>
 </body>
