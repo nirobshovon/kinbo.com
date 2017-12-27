f7257a11c1483f1cdd865aa956c3234c29216c85
Faisal Ibn Awal <majesticfais@gmail.com>
2017-12-27 22:21:23 +0600

    order
     data acess

---
+++ data/order_data_access.php
@@ -0,0 +1,46 @@
+<?php require_once "dbconnector.php"; ?>
+<?php
+	function addNewOrder($cus_id, $cus_add,$amount,$phn_num)
+	{
+		$query="INSERT INTO `order`(`customer_id`,`phn_num`, `customer_address`, `order_date`, `shipping_date`, `order_amount`, `delivery_status`)VALUES ('$cus_id','$phn_num', '$cus_add',curdate(), curdate(), '$amount', 'pending');";
+		$result = executeSQL($query);
+		return $result;
+	}
+
+	function findOrderByDvStatus($status)
+	{
+		$query="SELECT * FROM `order` WHERE delivery_status='$status'";
+		$result = executeSQL($query);
+		return $result;
+
+		$orders = array();
+		for($i=0; $row = mysqli_fetch_assoc($result); ++$i){
+			$orders[$i] = $row;
+		}
+
+		return $orders;
+	}
+
+	function findOrderByDvStatus2($status,$id)
+	{
+		$query="SELECT * FROM `order` WHERE delivery_status='$status' and customer_id=$id";
+		$result = executeSQL($query);
+		return $result;
+
+		$orders = array();
+		for($i=0; $row = mysqli_fetch_assoc($result); ++$i){
+			$orders[$i] = $row;
+		}
+
+		return $orders;
+	}
+
+	function deliverOrderById($id)
+	{
+		$query="update `order` set delivery_status='delivered' where id=$id";
+		$result = executeSQL($query);
+		return $result;
+	}
+
+
+?>
