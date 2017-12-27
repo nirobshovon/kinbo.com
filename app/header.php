7ee25ea1c6bdb5d52a50726d0d8beca32d177e14
Rabiuzzaman <rahatu666@gmail.com>
2017-12-27 12:33:28 +0600

    header added

---
+++ app/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<title>E-Commerse</title>
+</head>
+<body>
+	<header>
+
+			<div>
+				<center><h1><a href="index.php">Kinbo.com</a></h1></center>
+			</div>
+			<div >
+				<table width="100%">
+					<tr>
+						<td align="center"><a href="slider.php">Home</a></td>
+						<td align="center"><a href="index.php">Products</a></td>
+						<td align="center"><a href="">About</a></td>
+						<td align="center"><a href="">Contact </a></td>
+						<td align="center"><a href="cart.php">Cart </a></td>
+						<td align="center"><a href="login.php">Login</a></td>
+					</tr>
+				</table></br></br></br></br></br></br>
+		</div>
+	</header>
