0d0b16c861a35c29b3c3828cfab8e358db3e89a6
Sarwar Morshad <nirobshovon69@gmail.com>
2017-12-27 17:25:33 +0600

    edited

--- app/help.php
+++ app/help.php
@@ -20,63 +20,63 @@
 					</tr>
 				</table></br></br></br>
 				</fieldset>
-<fieldset>				
+<fieldset>
 		<table  width="100%">
 			<tr>
 				<td width="33%">
 				<a><img width="50px" height="50px" src="C:\xampp\htdocs\kinbo.com\image\productprices.png"></img>
 				</a>
-				
-				</br><a href="">Product & Prices</a>				
+
+				</br><a href="">Product & Prices</a>
 				</td>
-				
+
 				<td width="33%">
 				<a><img width="50px" height="50px" src="C:\xampp\htdocs\kinbo.com\image\order.png"></img>
 				</a>
-				
-				</br><a href="">Orders</a>				
+
+				</br><a href="">Orders</a>
 				</td>
-				
+
 				<td width="33%">
 				<a><img width="50px" height="50px" src="C:\xampp\htdocs\kinbo.com\image\payment.png"></img>
 				</a>
-				
-				</br><a href="">Payment</a>				
+
+				</br><a href="">Payment</a>
 				</td>
-				
+
 			</tr>
-			
+
 			<tr>
 				<td width="33%">
 				<a><img width="50px" height="50px" src="C:\xampp\htdocs\kinbo.com\image\Return.png"></img>
 				</a>
-				
-				</br><a href="">Return & Refunds</a>				
+
+				</br><a href="">Return & Refunds</a>
 				</td>
-				
+
 				<td width="33%">
 				<a><img width="50px" height="50px" src="C:\xampp\htdocs\kinbo.com\image\Delivery.png"></img>
 				</a>
-				
-				</br><a href="">Delivery</a>				
+
+				</br><a href="">Delivery</a>
 				</td>
-				
+
 				<td width="33%">
 				<a><img width="50px" height="50px" src="C:\xampp\htdocs\kinbo.com\image\Other.png"></img>
 				</a>
-				
-				</br><a href="">Other</a>				
+
+				</br><a href="">Other</a>
 				</td>
-				
+
 			</tr>
-			
+
 		</table>
 
 </fieldset>
 
 <fieldset>
 	<table border="1px" width="100%">
-		<tr > 
+		<tr >
 			<td>
 				<h4>Customer Service</h4></br>
 				<a href="">Help center</a></br>
@@ -86,17 +86,17 @@
 				<a href="">returns & Refunds</a></br>
 				<a href="">Contact us</a></br>
 			</td>
-			
-			<td> 
+
+			<td>
 				<h4>About Us</h4></br>
 				<a href="">About Kinbo.com</a></br>
 				<a href="">Corporate Sales</a></br>
 				<a href="">Terms And Conditions</a></br>
 				<a href="">Privacy Policy</a></br>
 				<a href="">Careers</a></br>
-				
+
 			</td>
-			
+
 			<td>
 				<h4>Join Us</h4></br>
 				<a><img width="32px" height="32px" src="C:\xampp\htdocs\kinbo.com\image\facebook.png"></img>
@@ -105,10 +105,10 @@
 				</a>
 				<a><img width="32px" height="32px" src="C:\xampp\htdocs\kinbo.com\image\twitter.png"></img>
 				</a>
-				
+
 			</td>
-			
-			
+
+
 		</tr>
 	</table>
 
